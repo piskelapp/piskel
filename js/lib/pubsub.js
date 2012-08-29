52a9d7b37959cf95e9d3b6757ba3c169fc533102
Vince <grosbouddha@gmail.com>
2012-08-30 00:48:22 +0200

    Adding query 1.8.0 and pubsub bus

---
+++ js/lib/pubsub.js
@@ -0,0 +1,21 @@
+/* jQuery Tiny Pub/Sub - v0.7 - 10/27/2011
+ * http://benalman.com/
+ * Copyright (c) 2011 "Cowboy" Ben Alman; Licensed MIT, GPL */
+
+(function($) {
+
+  var o = $({});
+
+  $.subscribe = function() {
+    o.on.apply(o, arguments);
+  };
+
+  $.unsubscribe = function() {
+    o.off.apply(o, arguments);
+  };
+
+  $.publish = function() {
+    o.trigger.apply(o, arguments);
+  };
+
+}(jQuery));
