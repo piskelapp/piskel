a88826aa1f09ed1576f6eb1735a90b9b47c12984
Vince <grosbouddha@gmail.com>
2012-09-15 02:02:02 +0200

    Remove code duplication between select tools

--- js/drawingtools/selectiontools/RectangleSelect.js
+++ js/drawingtools/selectiontools/RectangleSelect.js
@@ -14,81 +14,37 @@
 
 	pskl.utils.inherit(ns.RectangleSelect, ns.BaseSelect);
 	
+
 	/**
 	 * @override
 	 */
-	ns.RectangleSelect.prototype.applyToolAt = function(col, row, color, frame, overlay) {
-		this.startCol = col;
-		this.startRow = row;
-		
-		this.lastCol = col;
-		this.lastRow = row;
-		
-		// TODO(vincz): Comment here nasty vince
-		if(overlay.getPixel(col, row) != Constants.SELECTION_TRANSPARENT_COLOR) {
-			this.mode = "select";
-
-			// Drawing the first point of the rectangle in the fake overlay canvas:
-			overlay.setPixel(col, row, color);
-		}
-		else {
-			this.mode = "moveSelection";
-
-			this.overlayFrameReference = overlay.clone();
-		}
+	ns.RectangleSelect.prototype.onSelectStart_ = function (col, row, color, frame, overlay) {
+		// Drawing the first point of the rectangle in the fake overlay canvas:
+		overlay.setPixel(col, row, color);
 	};
 
-	ns.RectangleSelect.prototype.moveToolAt = function(col, row, color, frame, overlay) {
-		if(this.mode == "select") {
-			// Clean overlay canvas:
-			overlay.clear();
-
-			// When the user moussemove (before releasing), we dynamically compute the 
-			// pixel to draw the line and draw this line in the overlay :
-			var strokePoints = pskl.PixelUtils.getBoundRectanglePixels(this.startCol, this.startRow, col, row);
-			
-			color = Constants.SELECTION_TRANSPARENT_COLOR;
-			// Drawing current stroke:
-			for(var i = 0; i< strokePoints.length; i++) {
-				overlay.setPixel(strokePoints[i].col, strokePoints[i].row, color);
-			}
-		}
-		else if(this.mode == "moveSelection") {
-			
-			// TODO(vincz): Comment here nasty vince
-			var deltaCol = col - this.lastCol;
-			var deltaRow = row - this.lastRow;
-			
-			console.log(deltaCol)
-			console.log(deltaRow)
-			var colDiff = col - this.startCol, rowDiff = row - this.startRow;
-			if (colDiff != 0 || rowDiff != 0) {
-				// Update selection on overlay frame:
-				this.shiftOverlayFrame_(colDiff, rowDiff, overlay, this.overlayFrameReference);
-
-				// Update selection model:
-				$.publish(Events.SELECTION_MOVE_REQUEST, [deltaCol, deltaRow]);
-			}
-			this.lastCol = col;
-			this.lastRow = row;		
+	/**
+	 * When creating the rectangle selection, we clear the current overlayFrame and
+	 * redraw the current rectangle based on the orgin coordinate and
+	 * the current mouse coordiinate in sprite.
+	 * @override
+	 */
+	ns.RectangleSelect.prototype.onSelect_ = function (col, row, color, frame, overlay) {
+		overlay.clear();
+		if(this.startCol == col &&this.startRow == row) {
+			$.publish(Events.SELECTION_DISMISSED);
+		} else {
+			var selection = new pskl.selection.RectangularSelection(
+				this.startCol, this.startRow, col, row);
+			$.publish(Events.SELECTION_CREATED, [selection]);
 		}
 	};
 
 	/**
 	 * @override
 	 */
-	ns.RectangleSelect.prototype.releaseToolAt = function(col, row, color, frame, overlay) {		
-		if(this.mode == "select") {
-			overlay.clear();
-			if(this.startCol == col &&this.startRow == row) {
-				$.publish(Events.SELECTION_DISMISSED);
-			} else {
-				var selection = new pskl.selection.RectangularSelection(
-					this.startCol, this.startRow, col, row);
-				$.publish(Events.SELECTION_CREATED, [selection]);
-			}
-		} else if(this.mode == "moveSelection") {
-			this.moveToolAt(col, row, color, frame, overlay);
-		}
+	ns.RectangleSelect.prototype.onSelectEnd_ = function (col, row, color, frame, overlay) {
+		this.onSelect_(col, row, color, frame, overlay)
 	};
+
 })();
--- js/drawingtools/selectiontools/ShapeSelect.js
+++ js/drawingtools/selectiontools/ShapeSelect.js
@@ -14,62 +14,20 @@
 
 	pskl.utils.inherit(ns.ShapeSelect, ns.BaseSelect);
 	
-	/**
-	 * @override
-	 */
-	ns.ShapeSelect.prototype.applyToolAt = function(col, row, color, frame, overlay) {
-		this.startCol = col;
-		this.startRow = row;
-		
-		this.lastCol = col;
-		this.lastRow = row;
-		
-		// TODO(vincz): Comment here nasty vince
-		if(overlay.getPixel(col, row) != Constants.SELECTION_TRANSPARENT_COLOR) {
-			this.mode = "select";
-
-			$.publish(Events.SELECTION_DISMISSED);
-			var pixels = pskl.PixelUtils.getSimilarConnectedPixelsFromFrame(frame, col, row);
-			var selection = new pskl.selection.ShapeSelection(pixels);
-			$.publish(Events.SELECTION_CREATED, [selection]);
-		}
-		else {
-			this.mode = "moveSelection";
-
-			this.overlayFrameReference = overlay.clone();
-		}
-	};
-
-	ns.ShapeSelect.prototype.moveToolAt = function(col, row, color, frame, overlay) {
-		if(this.mode == "moveSelection") {
-			
-			// TODO(vincz): Comment here nasty vince
-			var deltaCol = col - this.lastCol;
-			var deltaRow = row - this.lastRow;
-			
-			console.log(deltaCol)
-			console.log(deltaRow)
-			var colDiff = col - this.startCol, rowDiff = row - this.startRow;
-			if (colDiff != 0 || rowDiff != 0) {
-				// Update selection on overlay frame:
-				this.shiftOverlayFrame_(colDiff, rowDiff, overlay, this.overlayFrameReference);
-
-				// Update selection model:
-				$.publish(Events.SELECTION_MOVE_REQUEST, [deltaCol, deltaRow]);
-			}
-			this.lastCol = col;
-			this.lastRow = row;		
-		}
-	};
 
 	/**
+	 * For the shape select tool, you just need to click one time to create a selection.
+	 * So we jsut need to implement onSelectStart_ (no need for onSelect_ & onSelectEnd_)
 	 * @override
 	 */
-	ns.ShapeSelect.prototype.releaseToolAt = function(col, row, color, frame, overlay) {		
-		if(this.mode == "select") {
-		  // TODO(vincz): define a way to dismiss the shape select
-		} else if(this.mode == "moveSelection") {
-			this.moveToolAt(col, row, color, frame, overlay);
-		}
+	ns.ShapeSelect.prototype.onSelectStart_ = function (col, row, color, frame, overlay) {
+		// Clean previous selection:
+		$.publish(Events.SELECTION_DISMISSED);
+		
+		// From the pixel cliked, get shape using an algorithm similar to the paintbucket one:
+		var pixels = pskl.PixelUtils.getSimilarConnectedPixelsFromFrame(frame, col, row);
+		var selection = new pskl.selection.ShapeSelection(pixels);
+		$.publish(Events.SELECTION_CREATED, [selection]);
 	};
+
 })();
