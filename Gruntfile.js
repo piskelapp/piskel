d794301301f20df071ccf06bfa4ba07405bdf0dc
grosbouddha <grosbouddha@gmail.com>
2013-05-26 23:58:59 +0200

    Initial Grunt architecture

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+grunt.loadNpmTasks('grunt-contrib-connect');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+//grunt.loadNpmTasks('grunt-contrib-nodeunit');
+grunt.loadNpmTasks('grunt-ghost');
+
+grunt.registerTask('test', ['jshint', /*'nodeunit',*/ 'connect', 'ghost']);
+
+jshint: {
+	/*options: {
+		jshintrc: '.jshintrc'
+	},*/
+	files: [
+		'Gruntfile.js',
+		'package.json',
+		'source/**/*.js'/*,
+		'<%= nodeunit.tests %>'*/
+	]
+},
+/*
+nodeunit: {
+	tests: ['tests/nodeunit/*_test.js']
+},
+*/
+connect: {
+	www: {
+		options: {
+			base: 'source',
+			port: 4545
+		}
+	}
+},
+ghost: {
+	test: {
+		files: [{
+			src: ['tests/integration/casperjs/*_test.js']
+		}]
+	},
+	options: {
+		args: {
+			baseUrl: 'http://localhost:' +
+				'<%= connect.www.options.port %>/'
+		},
+		direct: false,
+		logLevel: 'error',
+		printCommand: false,
+		printFilePaths: true
+	}
+}
