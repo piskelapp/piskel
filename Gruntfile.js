72a28f790546c69be3bbeeb4fd791415e6d090d3
grosbouddha <grosbouddha@gmail.com>
2013-05-27 00:02:48 +0200

    Formatting gruntfile correctly

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,48 +1,53 @@
-grunt.loadNpmTasks('grunt-contrib-connect');
-grunt.loadNpmTasks('grunt-contrib-jshint');
-//grunt.loadNpmTasks('grunt-contrib-nodeunit');
-grunt.loadNpmTasks('grunt-ghost');
-
-grunt.registerTask('test', ['jshint', /*'nodeunit',*/ 'connect', 'ghost']);
-
-jshint: {
-	/*options: {
-		jshintrc: '.jshintrc'
-	},*/
-	files: [
-		'Gruntfile.js',
-		'package.json',
-		'source/**/*.js'/*,
-		'<%= nodeunit.tests %>'*/
-	]
-},
-/*
-nodeunit: {
-	tests: ['tests/nodeunit/*_test.js']
-},
-*/
-connect: {
-	www: {
-		options: {
-			base: 'source',
-			port: 4545
-		}
-	}
-},
-ghost: {
-	test: {
-		files: [{
-			src: ['tests/integration/casperjs/*_test.js']
-		}]
-	},
-	options: {
-		args: {
-			baseUrl: 'http://localhost:' +
-				'<%= connect.www.options.port %>/'
+module.exports = function(grunt) {
+	grunt.initConfig({
+		jshint: {
+			/*options: {
+				jshintrc: '.jshintrc'
+			},*/
+			files: [
+				'Gruntfile.js',
+				'package.json',
+				'source/**/*.js'/*,
+				'<%= nodeunit.tests %>'*/
+			]
 		},
-		direct: false,
-		logLevel: 'error',
-		printCommand: false,
-		printFilePaths: true
-	}
-}
+		/*
+		nodeunit: {
+			tests: ['tests/nodeunit/*_test.js']
+		},
+		*/
+		connect: {
+			www: {
+				options: {
+					base: 'source',
+					port: 4545
+				}
+			}
+		},
+		ghost: {
+			test: {
+				files: [{
+					src: ['tests/integration/casperjs/*_test.js']
+				}]
+			},
+			options: {
+				args: {
+					baseUrl: 'http://localhost:' +
+						'<%= connect.www.options.port %>/'
+				},
+				direct: false,
+				logLevel: 'error',
+				printCommand: false,
+				printFilePaths: true
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-ghost');
+
+	grunt.registerTask('test', ['jshint', /*'nodeunit',*/ 'connect', 'ghost']);
+
+};
