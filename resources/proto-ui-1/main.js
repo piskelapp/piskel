0d0aedc93a249025bfeaa13cff25639f33bdd7e4
Vince <grosbouddha@gmail.com>
2013-06-04 01:05:01 +0200

    Iteration #2
    Adding background for draw canvas
    grid mod

--- resources/proto-ui-1/main.js
+++ resources/proto-ui-1/main.js
@@ -1,3 +1,27 @@
 $().ready(function(){
 
+
+	$(document).on('click', '.tile-close', function(evt) {
+		$(evt.target).closest('.preview-item').remove();
+	});
+
+	$(document).on('click', '.preview-item', function(evt) {
+		$('.preview-item').removeClass('selected');
+		$(evt.target).closest('.preview-item').addClass('selected');
+	});
+
+	$(document).on('click', '#toggle-mode', function(evt) {
+		$('body').toggleClass('grid-mode');
+	});
+/*
+	$(document).on('click', '#change-draw-area', function(evt) {
+		$('body').toggleClass('grid-mode');
+	});*/
+	$("#change-draw-area").change(function () {
+	  var str = "";
+	  $("#change-draw-area option:selected").each(function () {
+	            str = $(this).val();
+	  });
+	  $(".canvas .draw-area").attr('class', 'draw-area draw-area-'+ str);
+	})
 });
