0fe2e981b0bbcc1fe37dc92ee0ffe464f8d4d821
jdescottes <julian.descottes@gmail.com>
2013-06-19 00:29:07 +0200

    Reverted back to onchange due to lack of support of input range in firefox 21

--- js/controller/AnimatedPreviewController.js
+++ js/controller/AnimatedPreviewController.js
@@ -21,7 +21,7 @@
         // the oninput event won't work on IE10 unfortunately, but at least will provide a
         // consistent behavior across all other browsers that support the input type range
         // see https://bugzilla.mozilla.org/show_bug.cgi?id=853670
-        $("#preview-fps")[0].addEventListener('input', this.onFPSSliderChange.bind(this));
+        $("#preview-fps")[0].addEventListener('change', this.onFPSSliderChange.bind(this));
     };
 
     ns.AnimatedPreviewController.prototype.onFPSSliderChange = function (evt) {
