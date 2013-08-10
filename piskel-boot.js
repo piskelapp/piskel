2299f359778a8f19559f130deaf59a33d11b5c85
jdescottes <julian.descottes@gmail.com>
2013-08-11 01:26:38 +0200

    Introducing iframe based templates

---
+++ piskel-boot.js
@@ -0,0 +1,42 @@
+(function () {
+
+  var loadScript = function (src, callback) {
+    var script = window.document.createElement('script');
+    script.setAttribute('src',src);
+    script.setAttribute('onload',callback);
+    window.document.body.appendChild(script);
+  };
+
+  if (window.location.href.indexOf("debug") != -1) {
+    window.exports = {};
+    var scriptIndex = 0;
+    window.loadNextScript = function () {
+      if (scriptIndex == exports.scripts.length) {
+        pskl.app.init();
+        // cleanup
+        delete window.exports;
+        delete window.loadDebugScripts;
+        delete window.done;
+      } else {
+        loadScript(exports.scripts[scriptIndex], "loadNextScript()");
+        scriptIndex ++;
+      }
+    };
+    loadScript("script-load-list.js", "loadNextScript()");
+  } else {
+    var script;
+    if (window.location.href.indexOf("pack") != -1) {
+      script = "build/piskel-packaged.js";
+    } else {
+      script = "build/piskel-packaged-min.js";
+    }
+    var loaderInterval = window.setInterval(function () {
+      if (document.querySelectorAll("._ctl").length === 0) {
+        window.clearInterval(loaderInterval);
+        loadScript(script, "pskl.app.init()");
+      } else {
+        console.log("waiting for templates to load ....");
+      }
+    }, 100);
+  }
+})();
