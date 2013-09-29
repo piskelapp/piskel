b3d1cc5ea6c6cf44b9094aadb82a1ed536e3a2c8
jdescottes <julian.descottes@gmail.com>
2013-09-29 23:27:23 +0200

    Layers:Review:Added todos in cheap-templates.js

--- cheap-partials.js
+++ cheap-partials.js
@@ -1,11 +1,13 @@
-// SUPER CHEAP TEMPLATES !    
+// TODO : Move to js folder
+// TODO : Put under namespace
+// TODOC
 window._ctl = function (event) {
   var iframe=event.target || event.srcElement, div=document.createElement("div");
   div.innerHTML = iframe.contentWindow.document.body.innerHTML;
   if (div.children.length == 1) div = div.children[0];
   iframe.parentNode.replaceChild(div, iframe);
 };
-// cheap template prepare
+
 window._ctp = function (event) {
   var iframe=event.target || event.srcElement, script=document.createElement("script");
   script.setAttribute("type", "text/html");
