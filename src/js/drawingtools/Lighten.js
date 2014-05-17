7cbf2b2794b92c2d4e85173e62c710567c5d2ec3
jdescottes <julian.descottes@gmail.com>
2014-05-17 08:44:23 +0200

    Fix : Update tooltip text for lighten tool

--- src/js/drawingtools/Lighten.js
+++ src/js/drawingtools/Lighten.js
@@ -10,7 +10,7 @@
   ns.Lighten = function() {
     this.superclass.constructor.call(this);
     this.toolId = "tool-lighten";
-    this.helpText = "Lighten or ctrl:Darken";
+    this.helpText = "Lighten (hold ctrl for Darken)";
     this.step = 3;
     this.resetUsedPixels_();
   };
