b311312260319231665a545c233fa4a2efb2171a
jdescottes <julian.descottes@gmail.com>
2014-07-08 20:09:21 +0200

    Removed hack from ColorPicker class

--- src/js/drawingtools/ColorPicker.js
+++ src/js/drawingtools/ColorPicker.js
@@ -20,22 +20,10 @@
   ns.ColorPicker.prototype.applyToolAt = function(col, row, color, frame, overlay, event) {
     if (frame.containsPixel(col, row)) {
       var sampledColor = frame.getPixel(col, row);
-      if (event.ctrlKey) {
-        pskl.app.piskelController.getPiskel().getLayers().forEach(function (l) {
-          l.getFrames().forEach(function (f) {
-            f.forEachPixel(function (pixelColor,x,y) {
-              if (pixelColor === sampledColor) {
-                f.setPixel(x, y, pskl.app.paletteController.getPrimaryColor());
-              }
-            });
-          });
-        });
-      } else {
-        if (event.button == Constants.LEFT_BUTTON) {
-          $.publish(Events.SELECT_PRIMARY_COLOR, [sampledColor]);
-        } else if (event.button == Constants.RIGHT_BUTTON) {
-          $.publish(Events.SELECT_SECONDARY_COLOR, [sampledColor]);
-        }
+      if (event.button == Constants.LEFT_BUTTON) {
+        $.publish(Events.SELECT_PRIMARY_COLOR, [sampledColor]);
+      } else if (event.button == Constants.RIGHT_BUTTON) {
+        $.publish(Events.SELECT_SECONDARY_COLOR, [sampledColor]);
       }
     }
   };
