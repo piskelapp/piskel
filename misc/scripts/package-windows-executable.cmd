d31865a9eff93bd4f35f7ca6f231c1dce0424ca3
jdescottes <julian.descottes@gmail.com>
2015-04-26 22:46:40 +0200

    Update windows build scripts after nw version update

--- misc/scripts/package-windows-executable.cmd
+++ misc/scripts/package-windows-executable.cmd
@@ -6,14 +6,11 @@ SETLOCAL
   set PISKEL_HOME=%cd%
   POPD
 
-  set VBOX_PATH="C:\Program Files (x86)\Enigma Virtual Box"
   set RESOURCE_HACKER_PATH="C:\Program Files (x86)\Resource Hacker"
 
-  set APP_BIN="%PISKEL_HOME%\dest\desktop\cache\win\0.9.2"
   set MISC_FOLDER=%PISKEL_HOME%\misc
-  set RELEASES_FOLDER=%PISKEL_HOME%\dest\desktop\releases
-  set DEST_FOLDER=%RELEASES_FOLDER%\piskel\win\piskel
-
+  set RELEASES_FOLDER=%PISKEL_HOME%\dest\desktop
+  set DEST_FOLDER=%RELEASES_FOLDER%\piskel\win64
 
   ECHO "Updating Piskel icon -- Using Resource Hacker"
     %RESOURCE_HACKER_PATH%\ResHacker -addoverwrite "%DEST_FOLDER%\piskel.exe", "%DEST_FOLDER%\piskel-logo.exe", "%MISC_FOLDER%\desktop\logo.ico", ICONGROUP, IDR_MAINFRAME, 1033
