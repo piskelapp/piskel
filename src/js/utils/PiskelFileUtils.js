25ede9ffff9b98ed884d4a977aac3743d3c11be5
Julian Descottes <julian.descottes@gmail.com>
2016-12-13 11:00:58 +0100

    cleanup unused arguments in PiskelFileUtils

--- src/js/utils/PiskelFileUtils.js
+++ src/js/utils/PiskelFileUtils.js
@@ -5,7 +5,7 @@
     /**
      * Load a piskel from a piskel file.
      * After deserialization is successful, the provided success callback will be called.
-     * Success callback is expected to handle 3 arguments : (piskel:Piskel, descriptor:PiskelDescriptor, fps:Number)
+     * Success callback is expected to receive a single Piskel object argument
      * @param  {File} file the .piskel file to load
      * @param  {Function} onSuccess Called if the deserialization of the piskel is successful
      * @param  {Function} onError NOT USED YET
@@ -15,13 +15,13 @@
         var rawPiskel = pskl.utils.Base64.toText(content);
         ns.PiskelFileUtils.decodePiskelFile(
           rawPiskel,
-          function (piskel, descriptor, fps) {
+          function (piskel) {
             // if using Node-Webkit, store the savePath on load
             // Note: the 'path' property is unique to Node-Webkit, and holds the full path
             if (pskl.utils.Environment.detectNodeWebkit()) {
               piskel.savePath = file.path;
             }
-            onSuccess(piskel, descriptor, fps);
+            onSuccess(piskel);
           },
           onError
         );
@@ -30,11 +30,9 @@
 
     decodePiskelFile : function (rawPiskel, onSuccess, onError) {
       var serializedPiskel = JSON.parse(rawPiskel);
-      var fps = serializedPiskel.piskel.fps;
       var piskel = serializedPiskel.piskel;
-      var descriptor = new pskl.model.piskel.Descriptor(piskel.name, piskel.description, true);
       pskl.utils.serialization.Deserializer.deserialize(serializedPiskel, function (piskel) {
-        onSuccess(piskel, descriptor, fps);
+        onSuccess(piskel);
       });
     }
   };
