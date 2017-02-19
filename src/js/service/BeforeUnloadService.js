974450837ece6962141263c32c74ee630abd29b0
Julian Descottes <julian.descottes@gmail.com>
2017-02-19 13:19:09 +0100

    add confirmation message when closing desktop app with unsaved changes

--- src/js/service/BeforeUnloadService.js
+++ src/js/service/BeforeUnloadService.js
@@ -6,7 +6,27 @@
   };
 
   ns.BeforeUnloadService.prototype.init = function () {
+    if (pskl.utils.Environment.detectNodeWebkit()) {
+      // Add a dedicated listener to window 'close' event in nwjs environment.
+      var win = require('nw.gui').Window.get();
+      win.on('close', this.onNwWindowClose.bind(this, win));
+    }
+
     window.addEventListener('beforeunload', this.onBeforeUnload.bind(this));
+  };
+
+  /**
+   * In nw.js environment "onbeforeunload" is not triggered when closing the window.
+   * Polyfill the behavior here.
+   */
+  ns.BeforeUnloadService.prototype.onNwWindowClose = function (win) {
+    var msg = this.onBeforeUnload();
+    if (msg) {
+      if (!window.confirm(msg)) {
+        return false;
+      }
+    }
+    win.close(true);
   };
 
   ns.BeforeUnloadService.prototype.onBeforeUnload = function (evt) {
@@ -14,7 +34,10 @@
     if (pskl.app.savedStatusService.isDirty()) {
       var confirmationMessage = 'Your Piskel seems to have unsaved changes';
 
-      (evt || window.event).returnValue = confirmationMessage;
+      evt = evt || window.event;
+      if (evt) {
+        evt.returnValue = confirmationMessage;
+      }
       return confirmationMessage;
     }
   };
