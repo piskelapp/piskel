22a4a51366b1cabff7473ffb7ceee3a1b68b59a6
janczer <menshikov.ivn@gmail.com>
2018-09-26 17:15:03 +0200

    Refactoring. Create function to get current frame

--- src/js/controller/CursorCoordinatesController.js
+++ src/js/controller/CursorCoordinatesController.js
@@ -18,6 +18,7 @@
     $.subscribe(Events.DRAG_END, this.onDragEnd_.bind(this));
     $.subscribe(Events.FRAME_SIZE_CHANGED, this.redraw.bind(this));
     $.subscribe(Events.ZOOM_CHANGED, this.redraw.bind(this));
+    $.subscribe(Events.PISKEL_RESET, this.redraw.bind(this));
 
     this.redraw();
   };
@@ -45,15 +46,19 @@
       html += '<div class="drawing-zoom">x' + zoom + '</div>';
     }
 
-    this.coordinatesContainer.innerHTML = this.getFrameSizeHTML_() + html;
+    this.coordinatesContainer.innerHTML = this.getFrameSizeHTML_() + html + this.getCurrentFrameIndexHTML_();
+  };
+
+  ns.CursorCoordinatesController.prototype.getCurrentFrameIndexHTML_ = function () {
+    var currentFrameIndex = this.piskelController.getCurrentFrameIndex() + 1;
+    var frameCount = this.piskelController.getFrameCount();
+    return '<div class="frame-info">' + currentFrameIndex + '/' + frameCount + '</div>';
   };
 
   ns.CursorCoordinatesController.prototype.getFrameSizeHTML_ = function () {
     var w = this.piskelController.getWidth();
     var h = this.piskelController.getHeight();
-    var currentFrameIndex = this.piskelController.getCurrentFrameIndex() + 1;
-    var frameCount = this.piskelController.getFrameCount();
-    return '[' + w + 'x' + h + '] ' + currentFrameIndex + '/' + frameCount;
+    return '[' + w + 'x' + h + '] ';
   };
 
   ns.CursorCoordinatesController.prototype.onCursorMoved_ = function (event, x, y) {
