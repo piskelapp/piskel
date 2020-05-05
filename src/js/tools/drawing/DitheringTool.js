a3b6a6c564a8fbb52493a4029a43cf7585314f05
Jeff Wofford <github@jeffwofford.com>
2020-05-05 12:43:46 -0500

    Add 25% and 75% modifiers to dither tool.

--- src/js/tools/drawing/DitheringTool.js
+++ src/js/tools/drawing/DitheringTool.js
@@ -11,6 +11,11 @@
     this.toolId = 'tool-dithering';
     this.helpText = 'Dithering tool';
     this.shortcut = pskl.service.keyboard.Shortcuts.TOOL.DITHERING;
+
+    this.tooltipDescriptors = [
+      {key : 'ctrl', description : 'Use 25% dither'},
+      {key : 'shift', description : 'Use 75% dither'},
+    ];   
   };
 
   pskl.utils.inherit(ns.DitheringTool, ns.SimplePen);
@@ -35,6 +40,13 @@
 
   ns.DitheringTool.prototype.applyToolOnPixel = function(col, row, frame, overlay, event) {
     var usePrimaryColor = (col + row) % 2;
+
+    if( pskl.utils.UserAgent.isMac ? event.metaKey : event.ctrlKey ) {
+      usePrimaryColor = col%2 || row%2
+    }
+    else if( event.shiftKey ) {
+      usePrimaryColor = col%2 && row%2
+    }
 
     if (pskl.app.mouseStateService.isRightButtonPressed()) {
       usePrimaryColor = !usePrimaryColor;
