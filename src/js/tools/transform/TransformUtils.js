097bbec9128bace5a62378326beb49f7451599bb
Erik Nykwest <erik.nykwest@gmail.com>
2025-03-27 07:28:32 -0400

    - added fake rotateNearestNeighbor that is duplicate of rotate to   TransformUtils.js - added RotateNearestNeighbor.js (duplicate of Rotate.js)

--- src/js/tools/transform/TransformUtils.js
+++ src/js/tools/transform/TransformUtils.js
@@ -64,6 +64,56 @@
       return frame;
     },
 
+    /**
+     * DUPLICATE OF rotate TO TEST ADDING BUTTONS AND LINKING FUNCTIONS
+     * Rotates an ImageData object using Nearest Neighbor interpolation.
+     * @param {ImageData} srcData - The original image data.
+     * @param {number} angle - Rotation angle in degrees.
+     * @returns {ImageData} - Rotated image data.
+     */
+    CLOCKWISE : 'clockwise',
+    COUNTERCLOCKWISE : 'counterclockwise',
+    rotateNearestNeighbor : function (frame, direction) {
+      var clone = frame.clone();
+      var w = frame.getWidth();
+      var h = frame.getHeight();
+
+      var max =  Math.max(w, h);
+      var xDelta = Math.ceil((max - w) / 2);
+      var yDelta = Math.ceil((max - h) / 2);
+
+      frame.forEachPixel(function (color, x, y) {
+        var _x = x;
+        var _y = y;
+
+        // Convert to square coords
+        x = x + xDelta;
+        y = y + yDelta;
+
+        // Perform the rotation
+        var tmpX = x;
+        var tmpY = y;
+        if (direction === ns.TransformUtils.CLOCKWISE) {
+          x = tmpY;
+          y = max - 1 - tmpX;
+        } else if (direction === ns.TransformUtils.COUNTERCLOCKWISE) {
+          y = tmpX;
+          x = max - 1 - tmpY;
+        }
+
+        // Convert the coordinates back to the rectangular grid
+        x = x - xDelta;
+        y = y - yDelta;
+        if (clone.containsPixel(x, y)) {
+          frame.setPixel(_x, _y, clone.getPixel(x, y));
+        } else {
+          frame.setPixel(_x, _y, Constants.TRANSPARENT_COLOR);
+        }
+      });
+
+      return frame;
+    },
+
     getBoundaries : function(frames) {
       var minx = +Infinity;
       var miny = +Infinity;
